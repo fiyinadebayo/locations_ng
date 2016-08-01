f6e505718c5225bc8d38ded61874871054e15f66
ceemion <fiyinsimeon@gmail.com>
2016-08-01 12:02:01 +0100

    Add localities to LGAs.

--- lib/locations_ng/lga.rb
+++ lib/locations_ng/lga.rb
@@ -5,15 +5,37 @@ module LocationsNg
     end
 
     def self.lgas(state)
-      state = state.downcase.gsub(' ', '_')
+      query = format_query(state)
       all_lgas = load_lgas
 
-      lga_index = all_lgas.index{|l| l['alias'] == state}
+      lga_index = all_lgas.index{|l| l['state_alias'] == query}
 
       if lga_index.nil?
         {message: "No lgas found for '#{state}'", status: 404}
       else
         all_lgas[lga_index]['lgas']
+      end
+    end
+
+    def self.localities(state, lga)
+      return {message: 'You must enter a state and lga.', status: 500} unless state && lga
+
+      state_query = format_query(state)
+      lga_query = format_query(lga)
+      all_lgas = load_lgas
+
+      state_index = all_lgas.index{|s| s['state_alias'] == state_query}
+
+      if state_index
+        lga_index = all_lgas[state_index]['locality'].index{|l| l['lga_alias'] == lga_query}
+
+        if lga_index
+          all_lgas[state_index]['locality'][lga_index]['localities']
+        else
+          {message: "'#{lga}' LGA not found for '#{state}' state.", status: 404}
+        end
+      else
+        {message: "'#{state}' state not found.", status: 404}
       end
     end
 
@@ -25,6 +47,10 @@ module LocationsNg
 
     def self.files_location(file)
       File.expand_path("../locations/#{file}.yml", __FILE__)
+    end
+
+    def self.format_query(query)
+      query ? query.downcase.gsub(' ', '_') : query
     end
   end
 end
--- spec/locations_ng/lga_spec.rb
+++ spec/locations_ng/lga_spec.rb
@@ -15,18 +15,56 @@ module LocationsNg
     describe '.lgas' do
       context 'when lgas for a state is not found' do
         it 'returns an error message' do
-          expect(lga.lgas('Unknown State')).to eq({message: "No lgas found for 'unknown_state'", status: 404})
+          expect(lga.lgas('Unknown State')).to eq({message: "No lgas found for 'Unknown State'", status: 404})
         end
       end
 
       context 'when state is found' do
         it 'returns the lgas for state' do
-          expect(lga.lgas('Lagos')).to eq(['Agege', 'Ajeromi-Ifelodun', 'Alimosho',
-                                           'Amuwo-Odofin', 'Badagry', 'Apapa', 'Epe',
-                                           'Eti Osa', 'Ibeju-Lekki', 'Ifako-Ijaiye',
-                                           'Ikeja', 'Ikorodu', 'Kosofe', 'Lagos Island',
-                                           'Mushin', 'Lagos Mainland', 'Ojo', 'Oshodi-Isolo',
-                                           'Shomolu', 'Surulere Lagos State'])
+          expect(lga.lgas('lagos').count).to eq(20)
+          expect(lga.lgas('Lagos')).to eq(['Agege', 'Ajeromi/ifelodun', 'Alimosho', 'Amuwo-odofin', 'Apapa',
+                                           'Badagry', 'Epe', 'Eti-osa', 'Ibeju/lekki', 'Ifako/ijaiye',
+                                           'Ikeja', 'Ikorodu', 'Kosofe', 'Lagos Island', 'Lagos Mainland',
+                                           'Mushin', 'Ojo', 'Oshodi/isolo', 'Shomolu', 'Surulere'
+                                          ])
+        end
+      end
+    end
+
+    describe '.localities' do
+      context 'when no query is passed' do
+        it 'returns 500' do
+          expect(lga.localities(nil, nil)).to eq({message: 'You must enter a state and lga.', status: 500})
+        end
+
+        it 'returns 500' do
+          expect(lga.localities('Kano', nil)).to eq({message: 'You must enter a state and lga.', status: 500})
+        end
+
+        it 'returns 500' do
+          expect(lga.localities(nil, 'Aba South')).to eq({message: 'You must enter a state and lga.', status: 500})
+        end
+      end
+
+      context 'when state is not found' do
+        it 'returns an error message' do
+          expect(lga.localities('logos', 'South West')).to eq({message: "'logos' state not found.", status: 404})
+        end
+      end
+
+      context 'when lgas for a state is not found' do
+        it 'returns an error message' do
+          expect(lga.localities('Abia', 'Isiala Ngwa')).to eq({message: "'Isiala Ngwa' LGA not found for 'Abia' state.", status: 404})
+        end
+      end
+
+      context 'when localities are found for lga' do
+        it 'returns localities for LGAs in a state' do
+          expect(lga.localities('Abia', 'Aba North')).to eq(['Ariaria', 'Asaokpoja', 'Asaokpulor',
+                                                             'Eziama  ward', 'Industrial', 'Ogbor 1',
+                                                             'Ogbor 2', 'Old GRA', 'Osusu 1', 'Osusu 2',
+                                                             'St. Eugene', 'Umuogor', 'Umuola', 'Uratta'
+                                                            ])
         end
       end
     end
