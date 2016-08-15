aa7d48fd459647a0d381a608c0825ea28736f274
ceemion <fiyinsimeon@gmail.com>
2016-08-15 15:51:48 +0100

    Refactor City class to use new LoadFile class, and use instance vars.

--- lib/locations_ng/city.rb
+++ lib/locations_ng/city.rb
@@ -1,36 +1,27 @@
 module LocationsNg
   class City
-    @@all_cities
+    @all_cities = LoadFile.read('cities')
 
     class << self
       def all
-        load_cities
-      end
-
-      def state_query(state)
-        state == 'federal_capital_territory' ? 'fct' : state
+        @all_cities
       end
 
       def cities(state)
-        load_cities
-        state_query = self.state_query(state.downcase.gsub(' ', '_'))
-
-        city_index = @@all_cities.index{ |c| c['alias'] == state_query }
+        state_query = state_query(state.downcase.gsub(' ', '_'))
+        city_index = @all_cities.index{ |c| c['alias'] == state_query }
 
         unless city_index.nil?
-          return @@all_cities[city_index]['cities']
+          return @all_cities[city_index]['cities']
         end
 
         {message: "No cities found for '#{state}'", status: 404}
       end
 
       private
-      def load_cities
-        @@all_cities ||= YAML.load(File.read(files_location 'cities'))
-      end
 
-      def files_location(file)
-        File.expand_path("../locations/#{file}.yml", __FILE__)
+      def state_query(state)
+        state == 'federal_capital_territory' ? 'fct' : state
       end
     end
   end
